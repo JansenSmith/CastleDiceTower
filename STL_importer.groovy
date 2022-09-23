932fd77d78a6b7bd12c7d8c82aa8ad1733ec3d37
JansenSmith <jansen@bstarengineering.com>
2022-09-22 21:51:35 -0400

    Down res the base

--- STL_importer.groovy
+++ STL_importer.groovy
@@ -1,1 +1,8 @@
-//Your code here
+/// Import STLs from source
+
+CSG base  = Vitamins.get(ScriptingEngine.fileFromGit(
+				"https://github.com/JansenSmith/CastleDiceTower.git",
+				"Another_dice_tower/files/Base.stl"))
+				.setColor(javafx.scene.paint.Color.GRAY)
+
+return base
